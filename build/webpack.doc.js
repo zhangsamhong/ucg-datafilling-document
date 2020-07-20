29f66e060c9b289a38eb46d78fce2f5179ab5ee5
zhangsamhong <zhangsamhong@163.com>
2020-07-20 16:48:35 +0800

    update: 修改doc为docs

--- build/webpack.doc.js
+++ build/webpack.doc.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, '../doc'),
+        path: path.resolve(__dirname, '../docs'),
         publicPath: ''
     }
 };
